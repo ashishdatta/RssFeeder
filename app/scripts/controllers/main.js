da81f53246dc98051345f43e6ed7142a7e4f04f6
Rodrigo Savage <rodrigo.savage@intel.com>
2014-05-23 13:43:33 -0700

    eatting lunch

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,33 @@
 'use strict';
 
+var createTVArrayFromData = function(data){
+	var arr =[];
+	for (var i = data.feed.entry.length - 1; i >= 0; i--) {
+		var entry = data.feed.entry[i];
+		var obj ={};
+		obj.name = entry['im:name'].label;
+		obj.artist = entry['im:artist'].label;
+		obj.releaseDate = entry['im:releaseDate'].attributes.label;
+		arr.push(obj);
+	};
+	return arr;
+}
+
 angular.module('rssFeederApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.todos = ['Index 1', 'Index 2', 'Index 3','Index 5'];
+	/*
+	var parsedJSON = require('app/json/movie.js');
+	grunt.log.write('Hello');*/
+	var obj = jQuery.getJSON('https://itunes.apple.com/us/rss/toptvepisodes/limit=50/json', 
+		function( data ) {
+			var arr = createTVArrayFromData(data);
+			console.log(arr);
+			$scope.todos = [{title:'rodrigo',author:'Jojo'}];
+		});
+	//grunt.log.write('Hello');
+	var s = [];
+	s.push({title:'rodrigo',author:'Jojo'});
+	s.push({title:'rodrigo',author:'Jojo'});
+	$scope.todos = s;
 
   });
